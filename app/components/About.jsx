eca0bb67192c63f165c0bf0f5653674b74bdf6c5
JTTran <tranjt80@gmail.com>
2017-05-30 14:04:04 +0300

    Added grid system and style examples

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,10 @@
 var React = require("react");
 
+
 var About = function(props) {
     return (
         <div>
-            <h3>About </h3>
-            <p>Welcome to about page!</p>
+            <h1 className="text-center">About </h1>          
         </div>
     )
 };
